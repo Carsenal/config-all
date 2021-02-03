ea2506e13dda4cfa1eb0202a0d66541d91ce4554
Carson <c@rson.riker.me>
2021-02-03 10:07:43 -0500

    Did some more command def

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,34 @@
 
+function ls
+	exa $argv
+end
+
+function e
+	kak -clear
+	if test (count $argv) -eq 1
+		exec kak $argv
+	else if contains $argv[1] (kak -l)
+		exec kak -c $argv
+	else
+		exec kak -s $argv
+	end
+end
+
+function cdwd
+	z ~/proj/b
+end
+
+function prg
+	rg -t perl $argv
+end
+
+function bs
+	bsh dev
+end
+
+# Configs
+set -U editor kak
+set -U TZ /usr/share/zoneinfo/US/Eastern
+
 zoxide init fish | source
 starship init fish | source
